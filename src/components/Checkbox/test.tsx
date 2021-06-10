719f20f7bfbb88bcd26fc5496a79dff59da5644d
Wagner Carvalho <wagnercarvalho_ti@hotmail.com>
2021-06-09 23:34:26 -0300

    fix test units in component checkbox

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -37,5 +37,29 @@ describe('<Checkbox />', () => {
     await waitFor(() => {
       expect(onCheck).toHaveBeenCalledTimes(1)
     })
+    expect(onCheck).toHaveBeenCalledWith(true)
+  })
+
+  it('should dispatch checked status change', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="CheckBox" onCheck={onCheck} isChecked />)
+
+    expect(onCheck).not.toHaveBeenCalled()
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
+    })
+    expect(onCheck).toHaveBeenCalledWith(false)
+  })
+
+  it('should be accesible with tab', () => {
+    renderWithTheme(<Checkbox label="Checkbox" labelFor="Checkbox" />)
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+
+    expect(screen.getByLabelText(/checkbox/i)).toHaveFocus()
   })
 })
